c0bb45800341f85b8b8d3364726a6332d72b696d
AnitKrJha <anitjha31@gmail.com>
2022-05-05 20:37:15 +0530

    added drop down functionality in the side panel

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const menu=document.querySelector('.menu');
-const sidemenu=document.querySelector('.sidemenu');
-const closesidemenu=document.querySelector('.close');
+const menu = document.querySelector('.menu');
+const sidemenu = document.querySelector('.sidemenu');
+const closesidemenu = document.querySelector('.close');
+const featureList = document.querySelector('.featurelist');
+const companyList = document.querySelector('.companylist');
+const featureheading = document.querySelector('.featureheading');
+const companyheading = document.querySelector('.companyheading');
 
-menu.addEventListener('click',(e)=>{
+var companyup = false;
+var featureup = false;
+menu.addEventListener('click', (e) => {
     sidemenu.classList.toggle('hide');
 })
-closesidemenu.addEventListener('click',(e)=>{
+console.dir(companyheading);
+featureheading.addEventListener('click', (e) => {
+    featureList.classList.toggle('hide');
+    featureup = !featureup;
+    if (featureup)
+        featureheading.lastElementChild.src = "./images/icon-arrow-up.svg";
+    else
+        featureheading.lastElementChild.src = "./images/icon-arrow-down.svg";
+});
+companyheading.addEventListener('click', (e) => {
+    companyList.classList.toggle('hide');
+    companyup = !companyup;
+    if (companyup)
+        companyheading.lastElementChild.src = "./images/icon-arrow-up.svg";
+    else
+        companyheading.lastElementChild.src = "./images/icon-arrow-down.svg";
+});
+closesidemenu.addEventListener('click', (e) => {
     sidemenu.classList.toggle('hide');
 })
+console.dir(featureheading)
